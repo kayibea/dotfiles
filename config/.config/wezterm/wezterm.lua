d790bd5fb4b00f5d4a1f3bb15bed603b165320d6
kayibea <kayibea@proton.me>
2025-11-17 17:46:18 +0100

    feat: set wezterm max_fps  to 144
    fix(ghidra.desktop, jadx.desktop): update Exec path to use local/opt instead of .local/bin

--- config/.config/wezterm/wezterm.lua
+++ config/.config/wezterm/wezterm.lua
@@ -11,6 +11,9 @@ config.enable_kitty_keyboard = false
 config.enable_tab_bar = false
 config.adjust_window_size_when_changing_font_size = false
 
+config.max_fps = 144
+config.warn_about_missing_glyphs = false
+
 config.window_padding = {
   left = 0,
   right = 0,
@@ -20,21 +23,6 @@ config.window_padding = {
 
 -- config.disable_default_key_bindings = true
 config.keys = {
-  -- {
-  --   key = "Tab",
-  --   mods = "NONE",
-  --   action = wezterm.action.Multiple({
-  --     wezterm.action.SendKey({ key = "DownArrow" }),
-  --   }),
-  -- },
-  -- {
-  --   key = "Tab",
-  --   mods = "SHIFT",
-  --   action = wezterm.action.Multiple({
-  --     wezterm.action.SendKey({ key = "UpArrow" }),
-  --   }),
-  -- },
-  -- { key = 'U', mods = 'CTRL|SHIFT', action = wezterm.action.CharSelect },
   { key = 'L', mods = 'CTRL', action = wezterm.action.ShowDebugOverlay },
 }
 
